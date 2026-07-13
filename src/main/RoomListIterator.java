b55721d622f57ba3197e20f0e0d5a696e9449fa3
Ahlaam <ahlaam19@my.yorku.ca>
2026-07-13 18:59:11 -0400

    Implemented RoomListIterator for iterator pattern

--- src/main/RoomListIterator.java
+++ src/main/RoomListIterator.java
@@ -1,2 +1,36 @@
 //Concrete implementation of RoomIterator
+package main;
+import java.util.List;
 
+
+public class RoomListIterator implements RoomIterator{
+    private List<Room> rooms;
+    private int index;
+
+    public RoomListIterator(List<Room> rooms) {
+        this.rooms = rooms;
+        this.index = 0;
+        
+    }
+
+	
+	@Override
+	public Room getNext() {
+		// TODO Auto-generated method stub
+		if(hasMore()) {
+			return rooms.get(index++);
+		}
+		return null;
+		
+	}
+
+	@Override
+	public boolean hasMore() {
+		// TODO Auto-generated method stub
+		return  index< rooms.size();
+	}
+	
+	
+	
+	
+}
