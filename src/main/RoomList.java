2b225ad5ae9b3f32a2d3dee158c8f0697ffef23a
Ahlaam <ahlaam19@my.yorku.ca>
2026-07-13 18:59:27 -0400

    Implemented RoomList for iterator pattern

--- src/main/RoomList.java
+++ src/main/RoomList.java
@@ -1,1 +1,20 @@
-//Concrete implementation of RoomCollection
+// Concrete implementation of RoomCollection
+package main;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RoomList implements RoomCollection{
+	private List<Room> rooms = new ArrayList<>();
+	
+	@Override
+	public RoomIterator createIterator() {
+		// TODO Auto-generated method stub
+		return new RoomListIterator(rooms);
+	}
+	
+	public void addRoom(Room room ) {
+		rooms.add(room);
+		
+	}
+}
