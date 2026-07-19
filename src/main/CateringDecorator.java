e36ab5d982ed1842342fb8caa75830459397f751
abdullah998020-gif <abdullah998020@gmail.com>
2026-07-19 15:15:09 -0400

    CateringDecorator.java

--- src/main/CateringDecorator.java
+++ src/main/CateringDecorator.java
@@ -1,15 +1,24 @@
 package main;
 
 public class CateringDecorator extends BookingDecorator {
+    
     public CateringDecorator(Booking booking) {
         super(booking);
     }
 
+    /**
+     * Intercepts the original cost flow and polymorphically 
+     * adds a dynamic premium fee of $50.00 for the catering service.
+     */
     @Override
-    public double calculateTotalCost() {
-        return super.calculateTotalCost() + 50.00; // Adds $50 premium
+    public double cost() {
+        return super.cost() + 50.00;
     }
 
+    /**
+     * Intercepts the original description track to append 
+     * the catering metadata to the invoice summary.
+     */
     @Override
     public String getDescription() {
         return super.getDescription() + " + Premium Catering Add-on";
